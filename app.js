26db174f3f63d332a325cb41638c740f520bb0d2
Diego Zaragoza <viene_el_gol@hotmail.com>
2020-06-18 15:10:46 -0300

    add cors

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ app.use(bodyParser.json());
 
 
 //Configurar headers
+app.use( (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Allow', 'GET, POST, PUT, DELETE, OPTIONS');
+
+    next();
+})
 
 // Rutas base
 app.use('/api', user_routes);
