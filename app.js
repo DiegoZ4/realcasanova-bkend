5deb0b826d2f260c05adf0b57abc97998cbf97ca
Diego Zaragoza <viene_el_gol@hotmail.com>
2020-08-23 01:01:31 -0300

    que el señor se apiade de los cors

--- app.js
+++ app.js
@@ -20,11 +20,15 @@ app.use(bodyParser.json());
 //Configurar headers
 app.use( (req, res, next) => {
 
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    // res.header('Access-Control-Allow-Origin', '*');
+    // res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    // res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    // res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
 
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Credentials',true);
+    res.header('Access-Control-Allow-Methods','PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers','Content-Type');
 
     next();
 })
