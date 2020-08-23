a73f677f2fa973e12788a8c23aae3faa709e95ca
Diego Zaragoza <viene_el_gol@hotmail.com>
2020-08-23 00:43:26 -0300

    que el señor se apiade de los cors

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var path = require('path');
 
 var app = express();
 
-var cors = require('cors');
-
 //Rutas
 var user_routes = require('./routes/user');
 var clase_routes = require('./routes/clase');
@@ -26,8 +24,6 @@ app.use( (req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Access-Control-Allow-Credentials',true);
-    res.header('Access-Control-Allow-Headers','Content-Type');
 
 
     next();
--- routes/user.js
+++ routes/user.js
@@ -9,13 +9,11 @@ var md_auth = require('../middlewares/auntenticate');
 var multipart = require('connect-multiparty');
 var md_upload = multipart({ uploadDir: './uploads/users' });
 
-var cors = require('cors');
-
-api.get('/test',  md_auth.ensureAuth, userController.pruebas);
+api.get('/test', md_auth.ensureAuth, userController.pruebas);
 api.get('/users/:page?', md_auth.ensureAuth, userController.getUsers);
 api.get('/user/:id', md_auth.ensureAuth, userController.getUser);
 api.post('/register', userController.saveUser);
-api.post('/login', cors(), userController.loginUser);
+api.post('/login', userController.loginUser);
 api.put('/update-user/:id', md_auth.ensureAuth, userController.updateUser);
 api.post('/upload-image-user/:id', [md_auth.ensureAuth, md_upload], userController.uploadImage);
 api.get('/get-image-user/:imageFile', userController.getImageFile);
