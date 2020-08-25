71debdfc6aa9091ea9ceb39481ec46da1d90b76a
Diego Zaragoza <viene_el_gol@hotmail.com>
2020-08-25 20:33:10 -0300

    prueba body parser

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ var noticia_routes = require('./routes/noticia');
 var categoria_routes = require('./routes/categoria');
 var mp_routes = require('./routes/mp');
 
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use(bodyParser.json({limit: '100mb', extended: true}));
-app.use(bodyParser.urlencoded({limit: '100mb', extended: true}));
+// app.use(bodyParser.json({limit: '100mb', extended: true}));
+// app.use(bodyParser.urlencoded({limit: '100mb', extended: true}));
 
 app.use(express.json({limit: '100mb'}));
 app.use(express.urlencoded({limit: '100mb'}));
